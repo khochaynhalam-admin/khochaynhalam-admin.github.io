b829bff7c573d869966dee90adb1756a2b489077
Nguyễn Trung Nhân <nhanntdl98@gmail.com>
2020-10-14 11:28:19 +0700

    deploy

---
+++ js/chunk-2d0d63f1.f2907b1c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d63f1"],{7277:function(n,e,t){"use strict";t.r(e);var r=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},a=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h1",[n._v("Comming soon")])])}],u={name:"Dashboard",data:function(){return{}}},c=u,o=t("2877"),s=Object(o["a"])(c,r,a,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0d63f1.f2907b1c.js.map
